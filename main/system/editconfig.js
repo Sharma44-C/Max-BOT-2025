649401ba2aef2e093b2b6f9d257cd1dff9f22b42
MeowTzyッ <149583086+yuichi192168@users.noreply.github.com>
2025-04-17 22:48:26 +0800

    Update editconfig.js

--- main/system/editconfig.js
+++ main/system/editconfig.js
@@ -59,7 +59,7 @@ module.exports.createUser = async (name, userID, botName, botPrefix, username, p
     configBots.push({    
       "name": name,
       "uid": userID,
-      "botname": botName || "ryuko",
+      "botname": botName || "max",
       "prefix": botPrefix || "-",
       "admins": adminss,
       "username": username,
